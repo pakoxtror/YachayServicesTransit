f495d024c765e05140caa567d1d34794000cdd42
DonHachi <carlos.britog25@gmail.com>
2018-04-12 13:32:45 -0500

    Changes to be committed: 	new file:   src/assets/icon/Yachay_Tech_Logo.png 	new file:   src/assets/imgs/Yachay_Tech_Logo.png 	new file:   src/pages/walkthrough/walkthrough.html 	new file:   src/pages/walkthrough/walkthrough.scss 	new file:   src/pages/walkthrough/walkthrough.ts

---
+++ src/pages/walkthrough/walkthrough.ts
@@ -0,0 +1,57 @@
+import { Component, ViewChild } from '@angular/core';
+import { Slides, NavController } from 'ionic-angular';
+
+import {HomePage} from '../home/home';
+
+@Component({
+  selector: 'page-walkthrough',
+  templateUrl: 'walkthrough.html',
+})
+export class WalkthroughPage {
+	@ViewChild(Slides) slides: Slides;
+  showSkip = true;
+  dir: string = 'ltr';
+
+  slideList: Array<any> = [
+    {
+      title: "What is <strong>Yachay</strong>Services?",
+      description: "This app was made for all your needs in Yachay Tech!",
+      image: "assets/img/Yachay_Tech_Logo.png",
+    },
+    {
+      title: "Why Yachay Services?",
+      description: "To make buying and selling easier and faster!",
+      image: "assets/img/Yachay_Tech_Logo.png",
+    },
+    {
+      title: "Your delicious dish is coming!",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus, dui accumsan cursus lacinia, nisl risus.",
+      image: "assets/img/Yachay_Tech_Logo.png",
+    }
+  ];
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  onSlideNext() {
+    this.slides.slideNext(300)
+  }
+
+	onSlidePrev() {
+    this.slides.slidePrev(300)
+  }
+
+  onLastSlide() {
+  	this.slides.slideTo(3, 300)
+  }
+
+  openHomePage() {
+  	this.navCtrl.setRoot(HomePage);
+  }
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad WalkthroughPage');
+  }
+
+}
