a68fb7142b8869ac682b76b1cc7482ba71d06e74
hug0er <hugo.e.betancourt@gmail.com>
2018-05-06 18:42:58 -0500

    carrito de compras

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoryService } from '../../providers/category-service-mock'
 
 /**
  * Generated class for the CartPage page.
@@ -13,14 +14,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cart.html',
 })
 export class CartPage {
-  id_user : number;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    id_user : number;
+    public carritoList : any;
+    constructor(public navCtrl: NavController, public navParams: NavParams,public categoryService:CategoryService) {
     this.id_user = this.navParams.get('id_user');
-    console.log(this.id_user);
+    }
+  
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad cart-Page');
+      this.categoryService.getCarrito(this.id_user)
+      .then(
+        (data) => { // Success
+          this.carritoList=data;
+          console.log(this.carritoList);
+        },
+        (error) =>{
+          console.error(error);
+        }
+      )
+    }
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CartPage');
-  }
-
-}
+  
