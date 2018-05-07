107b038268736154ae41cf8686a397c99fffcfa6
hug0er <hugo.e.betancourt@gmail.com>
2018-05-06 23:01:00 -0500

    cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -14,24 +14,47 @@ import { CategoryService } from '../../providers/category-service-mock'
   templateUrl: 'cart.html',
 })
 export class CartPage {
+    aumento: number;
     id_user : number;
     public carritoList : any;
     constructor(public navCtrl: NavController, public navParams: NavParams,public categoryService:CategoryService) {
     this.id_user = this.navParams.get('id_user');
+    this.aumento = 0;
     }
   
-    ionViewDidLoad() {
+    ionViewDidEnter() {
       console.log('ionViewDidLoad cart-Page');
       this.categoryService.getCarrito(this.id_user)
       .then(
         (data) => { // Success
           this.carritoList=data;
-          console.log(this.carritoList);
         },
         (error) =>{
           console.error(error);
         }
       )
     }
+
+    add(i){
+      this.carritoList[i].cantidad = this.carritoList[i].cantidad + 1;
+    }
+
+    remove(i){
+      if (this.carritoList[i].cantidad - 1 < 0){
+        this.carritoList[i].cantidad = this.carritoList[i].cantidad;
+      }
+      else{
+        this.carritoList[i].cantidad = this.carritoList[i].cantidad - 1;
+      }
+    }
+
+    cant (i: number){
+      return this.carritoList[i].cantidad;
+    }
+
+    toTitleCase(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
+  
+}
   
