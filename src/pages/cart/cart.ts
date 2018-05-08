691e6e81e85fcec584bd9e3648b34dcf8e468fbe
hug0er <hugo.e.betancourt@gmail.com>
2018-05-08 02:46:35 -0500

    cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoryService } from '../../providers/category-service-mock'
 import { AlertController,LoadingController } from 'ionic-angular';
+import {ViewController} from 'ionic-angular';
 
 /**
  * Generated class for the CartPage page.
@@ -15,15 +16,15 @@ import { AlertController,LoadingController } from 'ionic-angular';
   templateUrl: 'cart.html',
 })
 export class CartPage {
+
     loading: any;
     aumento: number;
     id_user : number;
     public carritoList : any;
-    constructor(public navCtrl: NavController, public navParams: NavParams,public categoryService:CategoryService, private loadingController: LoadingController) {
+    constructor(private alertCtrl: AlertController, private viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams,public categoryService:CategoryService, private loadingController: LoadingController) {
     this.id_user = this.navParams.get('id_user');
     this.aumento = 0;
     }
-  
     ionViewDidEnter() {
       console.log('ionViewDidLoad cart-Page');
       this.categoryService.getCarrito(this.id_user)
@@ -77,8 +78,76 @@ export class CartPage {
           }
         )
     }
-    confirm(){
-
+    delete(i){
+      this.save()
+      this.loading= this.loadingController.create({
+        content : 'Cargando..'
+        //duration : 3000
+      });
+        this.loading.present();
+      var vacio
+        this.categoryService.deleteCart(this.carritoList[i])
+        .then(
+          (data) => { // Success
+            vacio=data;
+            this.loading.dismiss();
+          },
+          (error) =>{
+            console.error(error);
+            this.loading.dismiss();
+          }
+        )
+        this.carritoList.splice(i,1);
+    }
+    presentPrompt() {
+      let alert = this.alertCtrl.create({
+        title: 'Direccion de Entrega',
+        inputs: [
+          {
+            name: 'Dirección',
+            placeholder: 'Dirección'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancelar',
+            handler: data => {
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Confirmar',
+            handler: data => {
+              console.log('login clicked');
+              this.loading= this.loadingController.create({
+                content : 'Cargando..'
+                //duration : 3000
+              });
+                this.loading.present();
+              var vacio
+                this.categoryService.order(this.carritoList,data.Dirección)
+                .then(
+                  (data) => { // Success
+                    vacio=data;
+                    this.loading.dismiss();
+                    this.clear();
+                  },
+                  (error) =>{
+                    console.error(error);
+                    this.loading.dismiss();
+                  }
+                )
+            }
+          }
+        ]
+      });
+      alert.present();
+      
+    }
+    clear(){
+      this.carritoList.splice(0,Object.keys(this.carritoList).length);
     }
 }
+
   
