45c37b2b085d9acec0e9bb23bb0c4c780213055c
hug0er <hugo.e.betancourt@gmail.com>
2018-05-07 22:21:20 -0500

    cart changes

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoryService } from '../../providers/category-service-mock'
+import { AlertController,LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the CartPage page.
@@ -14,10 +15,11 @@ import { CategoryService } from '../../providers/category-service-mock'
   templateUrl: 'cart.html',
 })
 export class CartPage {
+    loading: any;
     aumento: number;
     id_user : number;
     public carritoList : any;
-    constructor(public navCtrl: NavController, public navParams: NavParams,public categoryService:CategoryService) {
+    constructor(public navCtrl: NavController, public navParams: NavParams,public categoryService:CategoryService, private loadingController: LoadingController) {
     this.id_user = this.navParams.get('id_user');
     this.aumento = 0;
     }
@@ -40,7 +42,7 @@ export class CartPage {
     }
 
     remove(i){
-      if (this.carritoList[i].cantidad - 1 < 0){
+      if (this.carritoList[i].cantidad - 1 < 1){
         this.carritoList[i].cantidad = this.carritoList[i].cantidad;
       }
       else{
@@ -56,5 +58,27 @@ export class CartPage {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   
+    save(){
+      this.loading= this.loadingController.create({
+        content : 'Cargando..'
+        //duration : 3000
+      });
+        this.loading.present();
+      var vacio
+        this.categoryService.updateCart(this.carritoList)
+        .then(
+          (data) => { // Success
+            vacio=data;
+            this.loading.dismiss();
+          },
+          (error) =>{
+            console.error(error);
+            this.loading.dismiss();
+          }
+        )
+    }
+    confirm(){
+
+    }
 }
   
